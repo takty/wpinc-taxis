0665245ef0e4569884c966d576f43fbb8fe504d9
Takuto Yanagida <yanagida@stxst.com>
2022-12-08 16:32:38 +0900

    Update gulp process

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,11 +2,8 @@
  * Gulp file
  *
  * @author Takuto Yanagida
- * @version 2022-03-23
+ * @version 2022-12-08
  */
-
-/* eslint-disable no-undef */
-'use strict';
 
 const SRC_JS_RAW  = ['src/**/*.js', '!src/**/*.min.js'];
 const SRC_JS_MIN  = ['src/**/*.min.js'];
@@ -15,15 +12,11 @@ const SRC_PO      = ['src/languages/**/*.po'];
 const SRC_JSON    = ['src/languages/**/*.json'];
 const DEST        = './dist';
 
-const gulp = require('gulp');
+import gulp from 'gulp';
 
-const { makeCopyTask }   = require('./task-copy');
-const { makeJsTask }     = require('./task-js');
-const { makeLocaleTask } = require('./task-locale');
-
-
-// -----------------------------------------------------------------------------
-
+import { makeJsTask } from './gulp/task-js.mjs';
+import { makeCopyTask } from './gulp/task-copy.mjs';
+import { makeLocaleTask }  from './gulp/task-locale.mjs';
 
 const js_raw  = makeJsTask(SRC_JS_RAW, DEST, 'src');
 const js_min  = makeCopyTask(SRC_JS_MIN, DEST);
@@ -41,5 +34,5 @@ const watch = done => {
 	done();
 };
 
-exports.build   = gulp.parallel(js, php, po, json);
-exports.default = gulp.series(exports.build , watch);
+export const build = gulp.parallel(js, php, po, json);
+export default gulp.series(build , watch);
